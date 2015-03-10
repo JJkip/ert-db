9cf4e1fd18dfe73b1e373ad8c005c0ddda12878b
JayJay <jjlangatjose@gmail.com>
2015-03-10 22:37:29 +0300

    First commit

---
+++ php/menu.php
@@ -0,0 +1,55 @@
+<nav class="navbar navbar-default navbar-fixed-top">
+		<div class="container">
+			<div class="navbar-header">
+				<div id="navbar">
+					<ul id="gn-menu" class="gn-menu-main nav">
+						<li class="gn-trigger pull-left">
+							<a class="gn-icon gn-icon-menu"><span>Menu</span></a>
+							<nav class="gn-menu-wrapper">
+								<div class="gn-scroller">
+									<ul class="gn-menu">
+										<li class="gn-search-item">
+											<input placeholder="Search" type="search" class="gn-search">
+											<a class="gn-icon gn-icon-search"><span>Search</span></a>
+										</li>
+										<li>
+											<a class="gn-icon gn-icon-download">Edit USSD Menu </a>
+											<!--<ul class="gn-submenu">
+											<li><a class="gn-icon gn-icon-illustrator">Latest Changes</a></li>
+											<li><a class="gn-icon gn-icon-photoshop">Sample USSD menu</a></li>
+											</ul>-->
+										</li>
+										<li>
+											<a class="gn-icon gn-icon-cog">Settings</a>
+										</li>
+										<li>
+											<a class="gn-icon gn-icon-help">Help</a>
+										</li>
+
+									</ul>
+								</div><!-- /gn-scroller -->
+							</nav>
+						</li>
+						<li>
+							<a href="javascript:;">Results</a>
+						</li>
+						<li>
+							<a class="" href="javascript:;">Users</a>
+						</li>
+						<li>
+							<a class="" href="javascript:;">Polling Stations</a>
+						</li>
+						<li>
+							<a class="" href="javascript:;">Candidates</a>
+						</li>
+						<li>
+							<a class="" href="javascript:;">Issues</a>
+						</li>
+						<li>
+							<a class="" href="javascript:;">logout</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</nav>
