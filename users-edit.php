51ff47a84203e34e621fa2d1625cddca43df508b
snakola <sanlynakola@gmail.com>
2015-03-13 15:00:39 +0300

    Added Candidates files

--- users-edit.php
+++ users-edit.php
@@ -14,6 +14,7 @@
 		<link rel="stylesheet" type="text/css" href="css/custom.css" />
 		<script src="js/modernizr.custom.js"></script>
 	</head>
+	<body id="users-edit">
 
 	<?php include "php/menu.php"; ?>
 
